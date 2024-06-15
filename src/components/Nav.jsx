9f0179fbba55d0c9414f3ff2f35af54ee871f17e
eji_a <156595423+e-aji@users.noreply.github.com>
2024-06-15 18:21:01 +0100

    changed some file names and worked on nav file

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,27 @@
 // Bringing in the required import from 'react-router-dom'
-import { Link } from 'react-router-dom';
-import Navbar from './UI/Navbar';
-
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 export default function Nav() {
   // The Navbar UI component will render each of the Link elements in the links prop
   return (
-    <Navbar
-      links={[
-        <Link key={1} className="nav-link text-light" to="/">
-          Home
-        </Link>,
-        <Link key={2} className="nav-link text-light" to="/about">
-          About Us
-        </Link>,
-      ]}
-    />
+    <Router>
+
+      <nav>
+      
+      <button>
+        <Link to="/">Home</Link>
+      </button>
+      <button>
+        <Link to="/about">About</Link>
+      </button>
+      <button>
+        <Link to="/portfolio">Portfolio</Link>
+      </button>
+      <button>
+        <Link to="/contact">Contact</Link>
+      </button>
+
+      </nav>
+
+    </Router>
   );
 }
